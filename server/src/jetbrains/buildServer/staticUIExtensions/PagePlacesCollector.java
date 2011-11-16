1b5d6ced2a8fe0e179b6c67ce55916051793c7ec
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-11-16 18:19:21 +0100

    fix project model

---
+++ server/src/jetbrains/buildServer/staticUIExtensions/PagePlacesCollector.java
@@ -0,0 +1,9 @@
+package jetbrains.buildServer.staticUIExtensions;
+
+/**
+ * @author Eugene Petrenko (eugene.petrenko@gmail.com)
+ *         Date: 16.11.11 18:14
+ */
+public class PagePlacesCollector {
+
+}
